28e79767c89668b0f44c1a22ec4586246802be06
cppenloglou <xrisostomospenloglou13@gmail.com>
2023-11-22 13:55:36 +0200

    Initial Start Screen - Login Setup

--- src/main/java/com/example/turbotyperacer/HelloApplication.java
+++ src/main/java/com/example/turbotyperacer/HelloApplication.java
@@ -2,18 +2,31 @@ package com.example.turbotyperacer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+        Parent root;
+
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Start Screen");
         stage.setScene(scene);
+        stage.setFullScreen(false);
+        stage.setResizable(true);
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         stage.show();
     }
 
